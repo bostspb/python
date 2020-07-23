e7caa223801f9bb8abf11e62ada3cc7b22424380
Zakhar <z.chernyak@filanco.ru>
2020-07-23 14:27:27 +0300

    lesson 04

--- lesson04/task05.py
+++ lesson04/task05.py
@@ -4,3 +4,13 @@
     Необходимо получить результат вычисления произведения всех элементов списка.
     Подсказка: использовать функцию reduce().
 """
+
+
+from functools import reduce
+
+
+def multiplication(prev_el, el):
+    return prev_el * el
+
+
+print(reduce(multiplication, [i for i in range(100, 1001) if i % 2 == 0]))
