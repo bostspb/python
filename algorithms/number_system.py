b721c93dad04db23497167d91346b136b471d03b
Zakhar <z.chernyak@filanco.ru>
2020-07-28 08:17:35 +0300

    ОчисÑ Ñ.ка мастера

--- algorithms/number_system.py
+++
@@ -1,6 +0,0 @@
-base = 2
-x = int(input())
-while x > 0:
-    digit = x % base
-    print(digit, end='')
-    x //= base
--- lesson01/task01.py
+++
@@ -1,7 +0,0 @@
-user_name = input('Как вас зовут? - ')
-user_age = int(input('Сколько вам лет? - '))
-user_city = input('В каком городе вы живете? - ')
-user_city_living = input(f'Как долго вы живете в городе {user_city}? - ')
-
-print(f"{user_name}, вам {user_age} лет и вы живете в городе {user_city} {user_city_living}. А еще вы {2020 - user_age} года рождения." )
-
